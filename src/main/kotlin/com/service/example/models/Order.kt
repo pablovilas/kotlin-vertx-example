f61c6eec10ca01c466f767f1a28ffba6a72701ca
Pablo Vilas <pablovilas89@gmail.com>
2019-08-04 23:46:31 -0300

    WIP: Ignores _id property at save

--- src/main/kotlin/com/service/example/models/Order.kt
+++ src/main/kotlin/com/service/example/models/Order.kt
@@ -1,10 +1,16 @@
 package com.service.example.models
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class Order (
+  @JsonProperty("_id")
+  @JsonInclude(Include.NON_NULL)
+  var _id: String?,
+
   @JsonProperty("id")
-  var id: String?,
+  var id: Long?,
 
   @JsonProperty("total")
   var total: Long?,
